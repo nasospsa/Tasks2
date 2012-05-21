20ba2bb16731ed198b4f1394402135172e1b045e
Nasos Psarrakos <psarrakos@gmail.copm>
2012-05-21 14:05:32 +0300

    DB refactoring

--- libs/Database.php
+++ libs/Database.php
@@ -1,15 +1,18 @@
 <?php
 
-class Database
-{	
+class Database {
     const server = 'localhost';
     const db = 'ganwebg1_tasksDB2';
     const user = 'ganwebg1_user';
     const pass = '@oQxERR{=f+P';
 
-    function Database()
-    {
-	mysql_connect(self::connection,self::db,self::user,self::pass);
-		
+    protected $_connection;
+
+    function Database() {
+        $this->_connection = mysql_connect(self::server, self::user, self::pass) or die(mysql_error());
+        mysql_select_db(self::db, $this->_connection) or die(mysql_error());
+        mysql_set_charset('utf8');
+        date_default_timezone_set('Europe/Athens');
     }
+
 }
